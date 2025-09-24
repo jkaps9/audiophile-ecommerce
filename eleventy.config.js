ebc5fb10c2b8ab3859b744c8ea93adb70f942f9d
Josh <15219491+jkaps9@users.noreply.github.com>
2025-09-24 00:54:38 -0400

    add all assets to config

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,15 +1,7 @@
 const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("bundle.css");
-  eleventyConfig.addPassthroughCopy(
-    "./src/assets/fonts/manrope-v20-latin-500.woff2"
-  );
-  eleventyConfig.addPassthroughCopy(
-    "./src/assets/fonts/manrope-v20-latin-700.woff2"
-  );
-  eleventyConfig.addPassthroughCopy(
-    "./src/assets/fonts/manrope-v20-latin-regular.woff2"
-  );
   eleventyConfig.addPlugin(eleventyImageTransformPlugin);
 };
