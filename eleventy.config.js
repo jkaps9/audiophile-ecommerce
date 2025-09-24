dae4ab1e0983526ba980536635f8931c8785089f
Josh <15219491+jkaps9@users.noreply.github.com>
2025-09-24 00:42:18 -0400

    add fonts

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,12 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("bundle.css");
+  eleventyConfig.addPassthroughCopy(
+    "./src/assets/fonts/manrope-v20-latin-500.woff2"
+  );
+  eleventyConfig.addPassthroughCopy(
+    "./src/assets/fonts/manrope-v20-latin-700.woff2"
+  );
+  eleventyConfig.addPassthroughCopy(
+    "./src/assets/fonts/manrope-v20-latin-regular.woff2"
+  );
 };
