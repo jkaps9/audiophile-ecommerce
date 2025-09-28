309c3ee5241d2ffd4b6c45f78726ff77a00302dd
Josh <15219491+jkaps9@users.noreply.github.com>
2025-09-27 20:33:26 -0400

    add sass and configuration

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,40 @@
+const path = require("node:path");
+const sass = require("sass");
+
 module.exports = (config) => {
+  // add SCSS template format
+  config.addTemplateFormats("scss");
+
   // Set directories to pass through to the dist folder
   config.addPassthroughCopy("./src/assets");
   config.addPassthroughCopy("./src/bundle.css");
+
+  // Configure SCSS files
+  config.addExtension("scss", {
+    outputFileExtension: "css",
+
+    // opt-out of Eleventy Layouts
+    useLayouts: false,
+
+    compile: async function (inputContent, inputPath) {
+      let parsed = path.parse(inputPath);
+      // Don’t compile file names that start with an underscore
+      if (parsed.name.startsWith("_")) {
+        return;
+      }
+
+      let result = sass.compileString(inputContent, {
+        loadPaths: [parsed.dir || ".", this.config.dir.includes],
+      });
+
+      // Map dependencies for incremental builds
+      this.addDependencies(inputPath, result.loadedUrls);
+
+      return async (data) => {
+        return result.css;
+      };
+    },
+  });
 
   return {
     markdownTemplateEngine: "njk",
